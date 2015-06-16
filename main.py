fb90afd8e585b0ada49b491aef4d5ff0af11dbbd
Emil <emil.neumann1web.de>
2015-06-16 18:52:29 +0200

    aktueller Go-Spiel Ordner

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from Tkinter import *
 
-mainGUI = Tk()
-mainGUI.geometry("480x480+400+50")
+class mainGUI():
+    
+    spielbrett = Canvas(bg="white")
+    
+    def __init__(self, master):
+        frame = Frame(master, width=500, height=500)
+        frame.pack()
+        self.spielbrett.place(x=50, y=50)
+        self.spielbrett.size = "400x400"
 
-mainGrid = Canvas(mainGUI, bg="white", height=380, width=380)
-mainGrid.place(x=50, y=50)
-
-mainGUI.mainloop()
-mainGUI.destroy()
+root = Tk()
+app = mainGUI(root)
+root.mainloop(0)
+#root.destroy()
