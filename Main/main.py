f33e7b65afd58865f314a587e0fbf5f26e692690
Emil <emil.neumann1web.de>
2015-06-18 20:49:28 +0200

    Spielbrett mit Linien

--- Main/main.py
+++ Main/main.py
@@ -2,15 +2,33 @@ from Tkinter import *
 
 class mainGUI():
     
-    spielbrett = Canvas(bg="white")
-    
     def __init__(self, master):
+        
         frame = Frame(master, width=500, height=500)
         frame.pack()
-        self.spielbrett.place(x=50, y=50)
-        self.spielbrett.size = "400x400"
+        
+        self.parent = master
+        self.parent.title("Go")
+        
+        self.spielbrett = Canvas(frame, bg="white")
+        self.draw()
 
-root = Tk()
-app = mainGUI(root)
-root.mainloop(0)
-#root.destroy()
+    def draw(self):
+        i = 0
+        while i < 19:
+            i = i + 1
+            self.spielbrett.create_line(i*20, 20, i*20, 380)
+        i = 0
+        while i < 19:
+            i = i + 1
+            self.spielbrett.create_line(20, i*20, 380, i*20)
+        
+        self.spielbrett.place(x=50, y=50, width=400, height=400)
+
+def main():
+    root = Tk()
+    app = mainGUI(root)
+    root.mainloop()
+    
+if __name__ == '__main__':
+    main()
