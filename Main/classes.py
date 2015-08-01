0eae7a7d97f5d450402021004792d2f11dd7e474
Emil <emil.neumann1web.de>
2015-08-01 17:12:03 +0200

    Fehler behoben, der das Spiel verlangsamt hat. Für eine Alternative ist eine Klassendatei vorbereitet. Zug absenden wurde vorbereitet, Button hinzugefügt.

---
+++ Main/classes.py
@@ -0,0 +1,12 @@
+'''
+Created on 24.07.2015
+
+@author: Emil
+'''
+from Tkinter import Widget
+
+class Stein(Widget):
+    def __init__(self):
+        
+        self.visible = False
+        self.color = 0
--- Main/main.py
+++ Main/main.py
@@ -1,4 +1,5 @@
 from Tkinter import *
+#from classes import *
 
 class mainGUI():
     """
@@ -7,50 +8,62 @@ class mainGUI():
     """
     def __init__(self, master):                     #Klassenkonstruktor, methode zum generieren des Fensters
         
-        self.frame = Frame(master, width=500, height=500)#Festlegung der Groesse
+        self.frame = Frame(master, width=500, height=600)#Festlegung der Groesse
         self.frame.pack()                                #Platzierung des Fensters auf dem Bildschirm
         self.parent = master
         self.parent.title("Go")                     #Festlegung des Namen des Fensters
         self.initializeComponents()
         
     def initializeComponents(self):
+        """
+        Methode, die für die Initialisierung der Komponenten zuständig ist
+        """
+        self.fertigButton = Button(self.frame, text="Fertig")
+        self.fertigButton.place(x=200, y=500, height=50, width=100)
+        self.fertigButton.bind("<Button-1>", self.zug_senden)
         
-        self.spielbrett = Canvas(self.frame, bg="white") #Definition des Spielbretts
-        self.spielbrett.bind("<Button-1>", self.callback)
         self.spielfeldstatus = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+            [0]*19, [0]*19, [0]*19, [0]*19, [0]*19,
+            [0]*19, [0]*19, [0]*19, [0]*19, [0]*19,
+            [0]*19, [0]*19, [0]*19, [0]*19, [0]*19,
+            [0]*19, [0]*19, [0]*19, [0]*19]
+        
         self.draw()                                     #Anzeigen des Spielbretts
     
-    def callback(self, event):
+    def setStone(self, event):
+        """
+        Methode zum setzen der Steine
+        """
         x = (event.x - 10) / 20
         y = (event.y - 10) / 20
-        if self.spielfeldstatus[x][y] == 0:
-            self.spielfeldstatus[x][y] = 1
+        if (0 <= x < 19) and (0 <= y < 19):
+            if self.spielfeldstatus[x][y] == 0:
+                self.spielfeldstatus[x][y] = 2      #setzt einen weißen Stein
         self.draw()
-
+    
+    def setBack(self, event):
+        """
+        mit dieser Methode kann man Steine zurücknehmen,
+        wenn man sie versehentlich gesetzt hat
+        """
+        x = (event.x - 10) / 20
+        y = (event.y - 10) / 20
+        if (0 <= x < 19) and (0 <= y < 19):
+            if 1 == 1:
+                self.spielfeldstatus[x][y] = 0      #löscht einen gesetzten Stein
+        self.draw()
+        
     def draw(self):
+        """
+        Methode zum Zeichnen des Spielfelds mit Steinen
+        """
+        self.spielbrett = Canvas(self.frame, bg="white") #Definition des Spielbretts
+        self.spielbrett.bind("<Button-1>", self.setStone)
+        self.spielbrett.bind("<Button-3>", self.setBack)
         i = 0
         while i < 19:                               #Schleife zum Zeichnen der senkrechten Linien
             i += 1
-            self.spielbrett.create_line(i*20, 20, i*20, 380)
+            self.spielbrett.create_line(i*20, 20, i*20, 381)
         i = 0
         while i < 19:                               #Schleife zum Zeichnen der waagerechten Linien
             i += 1
@@ -59,14 +72,28 @@ class mainGUI():
         while x < 19:
             y = 0
             while y < 19:
+                if self.spielfeldstatus[x][y] == 2:
+                    self.spielbrett.create_oval(x*20+11, y*20+11, x*20+29, y*20+29)     #zeichnet eine leere Elipse bei weißen Steinen
                 if self.spielfeldstatus[x][y] == 1:
-                    self.spielbrett.create_oval(x*20+11, y*20+11, x*20+29, y*20+29)
+                    pass                                                                #macht nichts, wenn ein schwarzer Stein vorhanden ist
                 y += 1
             x += 1
         
         self.spielbrett.place(x=50, y=50, width=400, height=400)    #Platzierung des Spielbretts im Fenster
 
+    def zug_senden(self, event):
+        """
+        Methode zum senden der Daten:
+        -ob ein Stein gesetzt wurde; wenn ja,
+        -wo der Stein gesetzt wurde (Koordinaten) und ggf.
+        -welche Farbe der Stein hat
+        """
+        pass                                        #macht erst mal nichts
+    
 def main():
+    """
+    Methode, die das Programm laufen lässt
+    """
     root = Tk()
     app = mainGUI(root)
     root.mainloop()
